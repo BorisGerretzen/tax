6a137809e0bd8d379f5f0e78d50151886f2bcfdf
Boris <borisgerretzen@gmail.com>
2023-12-19 22:35:08 +0100

    improve table

--- src/components/incomeTaxTable.tsx
+++ src/components/incomeTaxTable.tsx
@@ -6,20 +6,40 @@ export type IncomeTaxTableProps = {
     result: IncomeTaxResult
 }
 
-const formatCurrency = (amount: string | number) => {
-    return `€${amount}`
+const formatCurrency = (amount: number) => {
+    const {locale} = useLocalization()
+
+    if (amount < 0) {
+        return `-€${Math.abs(amount).toLocaleString(locale)}`
+    }
+
+    return `€${amount.toLocaleString(locale)}`
 }
 
-const Row = ({label, value}: { label: string, value: number }) => {
-    const {locale} = useLocalization()
+const Row = (props: {
+    label: string,
+    value: number,
+    difference?: number,
+    formatter?: (amount: number) => string
+}) => {
+    const formatter = props.formatter || formatCurrency
 
     return (
         <div className="row">
-            <div className="col-8">
-                <b>{label}</b>
+            <div className="col-7 ">
+                <b>{props.label}</b>
             </div>
-            <div className="col-4">
-                {formatCurrency(value.toLocaleString(locale))}
+            <div className="col-5 ">
+                <div class="row">
+                    <div class={props.difference ? "col-6" : "col-12"}>
+                        {formatter(props.value)}
+                    </div>
+                    {props.difference ?
+                        <div class="col-6">
+                            <span style={{color: "red"}}> {formatter(props.difference)}</span>
+                        </div>
+                        : null}
+                </div>
             </div>
         </div>
     )
@@ -38,9 +58,18 @@ export const IncomeTaxTable = ({input, result}: IncomeTaxTableProps) => {
             </div>
             <div class="col-md-6 col-12 mt-2 mt-md-0">
                 <h3>{getString("net")}</h3>
-                <Row label={getString("yearlyIncome")} value={result.netYearlyIncome}/>
-                <Row label={getString("monthlyIncome")} value={result.netMonthlyIncome}/>
-                <Row label={getString("holidayAllowanceAndBonus")} value={result.netHolidayAllowanceAndBonus}/>
+                <Row label={getString("yearlyIncome")} value={result.netYearlyIncome}
+                     difference={result.netYearlyIncome - result.grossYearlyIncome}
+                />
+                <Row label={getString("monthlyIncome")} value={result.netMonthlyIncome}
+                     difference={result.netMonthlyIncome - input.incomePerMonth}
+                />
+                <Row label={getString("holidayAllowanceAndBonus")} value={result.netHolidayAllowanceAndBonus}
+                     difference={result.netHolidayAllowanceAndBonus - result.grossHolidayAllowanceAndBonus}
+                />
+                <Row label={getString("taxPercentage")}
+                     value={100 - result.netYearlyIncome / result.grossYearlyIncome * 100}
+                     formatter={amount => amount.toFixed(2) + "%"}/>
             </div>
         </div>
     )
